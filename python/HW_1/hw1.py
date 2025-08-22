e1c52a3b82f4773922f0cf0efa2ba8cdd957a716
DESKTOP-0L4FPCV\Izbasar <onalbaevaaruzan@gmail.com>
2025-08-22 23:03:57 +0500

    permutation done+hw 8

---
+++ python/HW_1/hw1.py
@@ -0,0 +1,11 @@
+# ------------ Task 1: Сложение 2-х чисел ------------
+a = 3
+b = 5
+
+c = a + b
+print(c)
+
+
+# ------------ Task 2: I love or hate Python? ------------
+
+print('I love Python')
---
+++ python/HW_5/hw5.py
@@ -0,0 +1,30 @@
+# --------- 1. Откройте файл example.txt и выведите его содержимое на экран.
+file = open(r"python\HW_5\example.txt", "r", encoding="utf-8")
+content = file.read()
+print(content)
+file.close()
+
+# # --------- 2. Создайте файл output.txt и запишите в него строку "Hello, World!"
+# with open("python\HW_5\output.txt", "w", encoding="utf-8") as file:
+#     file.write("Hello, World!")
+
+# # --------- 3. Напишите программу, которая считает количество строк, слов и символов в заданном текстовом файле и выводит результаты.
+# myfile = "python\HW_5\example1.txt"
+
+# with open(myfile, "r", encoding="utf-8") as file:
+#     content = file.read()
+
+# # количество строк
+# lines = content.splitlines()
+# num_lines = len(lines)
+
+# # количество слов
+# words = content.split()
+# num_words = len(words)
+
+# # количество символов
+# num_chars = len(content)
+
+# print("Количество строк:", num_lines)
+# print("Количество слов:", num_words)
+# print("Количество символов:", num_chars)
